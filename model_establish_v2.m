e8e84f08320c7963a3955999f6970d47e9bc84c1
kxxs <494128792@qq.com>
2019-07-11 15:44:52 +0800

    Time Periods Added

--- model_establish_v2.m
+++ model_establish_v2.m
@@ -1,7 +1,31 @@
 % version 2 : energy storage added
+% Demand of W,R,Q, 按照BT由小到大排
+Time = 1:24;
+          %1    2   3   4   5   6   7   8   9   10  11  12  13  14  15  16
+          %17   18  19  20  21  22  23  24
+Solar  = [ 0,   0,  0,  0,  0,  3,  5,  16, 26, 34, 38, 40, 42, 40, 36, 26,...
+           12,  4,  1,  0,  0,  0,  0,  0;
+          ]*2.5;
+Demand = [45,   47, 46, 44, 45, 53, 56, 77, 80, 92, 95, 94, 85, 83, 84, 82,...
+          89,   101,110,115,110,90, 70, 50;   % W (electricity)
+          17,   16, 16, 16, 16, 18, 20, 25, 25, 27, 30, 37, 35, 34, 33, 32....
+          30,   29, 29, 29, 27, 23, 23, 24;   % R (cold)
+          30,   30, 30, 30, 32, 32, 36, 40, 42, 35, 32, 28, 16, 17, 21, 23,...
+          28,   32, 25, 26, 27, 30, 32, 35;   % Q (head)
+          ]; 
+figure(1)
+plot(Time,Solar,'-*');
+hold on
+plot(Time,Demand(1,:),'-.');
+hold on
+plot(Time,Demand(2,:));
+hold on
+plot(Time,Demand(3,:));
+legend('solar','elec','cold','heat')
 
-Demand = [100;50;30]; % Demand of W,R,Q, 按照BT由小到大排
-
+Gas_In = zeros(1,24);
+Elec_In = zeros(1,24);
+%%
 %%%%%%%%%%%%%%%% Branch Matrix (based on the paper) %%%%%%%%%%%%%%%%%%%%%%%%%%
 % BT(Branch Type): 1-W;  2-R(cold);  3-Q;  4-F(gas) 5-solar 6-wind
 % s: start node;
@@ -37,6 +61,7 @@ Branch_Num = max(Branch(:,1));
 Input_Num = length(Branch(Branch(:,s)==-1,1)); % input num of the energy hub
 Output_Num = length(Branch(Branch(:,t)==0,1));
 
+%%
 %%%%%%%%%%%%%% Node Matrix %%%%%%%%%%%%%%%%%
 NT = 2; p1 = 3; p2 = 4;
 % Node = [
@@ -57,91 +82,109 @@ Node = [
    4    1    0.90    0;
    5    1    0.30    0;
        ];
-   
+
+   %%
 %%%%%%%%%%%%%%%%%% Main %%%%%%%%%%%%%%%%%%%%%%%%%
-Node_Num = max(Node(:,1));
-Z = [];
-for i = 1:Node_Num
-    % find branches pointing into and from current node
-    In_B = Branch(Branch(:,t)==i,:); In_B_backup = In_B;
-    Out_B = Branch(Branch(:,s)==i,:); Out_B_backup = Out_B;
-    % find num of different energy type
-    In_Dim = length(unique(In_B(:,BT)));
-    Out_Dim = length(unique(Out_B(:,BT)));
-    % construct H and A matrix
-    H = zeros(In_Dim * Out_Dim, In_Dim + Out_Dim);
-    A = zeros(In_Dim + Out_Dim, Branch_Num);
-    % fill the elements of A
-    for A_idx = 1:length(A(:,No))
-        if(isempty(In_B)==0) % fill input energy into matrix A first
-            % ET: energy type
-            ET = In_B(1,BT);
-            A(A_idx, In_B(In_B(:,BT)==ET,No)) = 1;
-            In_B(In_B(:,BT)==ET,:) = [];
-        else
-            ET = Out_B(1,BT);
-            A(A_idx, Out_B(Out_B(:,BT)==ET,No)) = -1;
-            Out_B(Out_B(:,BT)==ET,:) = [];
-        end
-    end
-    % fill the elements of H (preparation)
-    In_B = In_B_backup;
-    Out_B = Out_B_backup;
-    % list of in/out energy type
-    In_temp=In_B(:,BT); In_temp = In_temp(end:-1:1);
-    Out_temp=Out_B(:,BT); Out_temp = Out_temp(end:-1:1);
-    [In_List, In_rank] = unique(In_temp); 
-    [Out_List, Out_rank] = unique(Out_temp);  
-    In_List = In_temp(sort(In_rank)); In_List = In_List(end:-1:1); % keep the index order, Branch index小的能量形式在前，index 大的在后
-    Out_List = Out_temp(sort(Out_rank)); Out_List = Out_List(end:-1:1);
-    % fill the elements of H 
-    H_Idx = 1; Param_Idx = p1;
-    for j = 1:length(In_List)
-        for k = 1:length(Out_List)
-            if(Node(i,NT)==1)
-                H(H_Idx,j) = Node(Node(:,1)==i,Param_Idx);
-                H(H_Idx,k + length(In_List)) = 1;
-                H_Idx = H_Idx + 1;
-                Param_Idx = Param_Idx + 1;
+for hour = 1:24
+    Node_Num = max(Node(:,1));
+    Z = [];
+    for i = 1:Node_Num
+        % find branches pointing into and from current node 
+        In_B = Branch(Branch(:,t)==i,:); In_B_backup = In_B;
+        Out_B = Branch(Branch(:,s)==i,:); Out_B_backup = Out_B;
+        % find num of different energy type
+        In_Dim = length(unique(In_B(:,BT)));
+        Out_Dim = length(unique(Out_B(:,BT)));
+        % construct H and A matrix
+        H = zeros(In_Dim * Out_Dim, In_Dim + Out_Dim);
+        A = zeros(In_Dim + Out_Dim, Branch_Num);
+        % fill the elements of A
+        for A_idx = 1:length(A(:,No))
+            if(isempty(In_B)==0) % fill input energy into matrix A first
+                % ET: energy type
+                ET = In_B(1,BT);
+                A(A_idx, In_B(In_B(:,BT)==ET,No)) = 1;
+                In_B(In_B(:,BT)==ET,:) = [];
+            else
+                ET = Out_B(1,BT);
+                A(A_idx, Out_B(Out_B(:,BT)==ET,No)) = -1;
+                Out_B(Out_B(:,BT)==ET,:) = [];
             end
-            %%% 这里考虑H矩阵每一行都是一种输入(系数为eta)、一种输出（系数为1），其余都是0，不知道有没有问题
-            %%% 然后把j种输入,k种输出的j*k个组合枚举了一遍。
         end
+        % fill the elements of H (preparation)
+        In_B = In_B_backup;
+        Out_B = Out_B_backup;
+        % list of in/out energy type
+        In_temp=In_B(:,BT); In_temp = In_temp(end:-1:1);
+        Out_temp=Out_B(:,BT); Out_temp = Out_temp(end:-1:1);
+        [In_List, In_rank] = unique(In_temp); 
+        [Out_List, Out_rank] = unique(Out_temp);  
+        In_List = In_temp(sort(In_rank)); In_List = In_List(end:-1:1); % keep the index order, Branch index小的能量形式在前，index 大的在后
+        Out_List = Out_temp(sort(Out_rank)); Out_List = Out_List(end:-1:1);
+        % fill the elements of H 
+        H_Idx = 1; Param_Idx = p1;
+        for j = 1:length(In_List)
+            for k = 1:length(Out_List)
+                if(Node(i,NT)==1)
+                    H(H_Idx,j) = Node(Node(:,1)==i,Param_Idx);
+                    H(H_Idx,k + length(In_List)) = 1;
+                    H_Idx = H_Idx + 1;
+                    Param_Idx = Param_Idx + 1;
+                end
+                %%% 这里考虑H矩阵每一行都是一种输入(系数为eta)、一种输出（系数为1），其余都是0，不知道有没有问题
+                %%% 然后把j种输入,k种输出的j*k个组合枚举了一遍。
+            end
+        end
+        Z = [Z;H*A];
     end
-    Z = [Z;H*A];
+
+    %% 
+    % Construct X and Y
+    Input_BT = unique(Branch(Branch(:,s)==-1,BT));
+    Output_BT = unique(Branch(Branch(:,t)==0,BT));
+
+    X = zeros(length(Input_BT), Branch_Num);
+    Y = zeros(length(Output_BT), Branch_Num);
+
+    for i = 1:length(Input_BT)
+        X(i,intersect(Branch(Branch(:,s)==-1),Branch(Branch(:,BT)== Input_BT(i)))) = 1;
+    end
+
+    for i = 1:length(Output_BT)
+        Y(i,intersect(Branch(Branch(:,t)==0),Branch(Branch(:,BT)== Output_BT(i)))) = 1;
+    end
+
+    V = sdpvar(length(Branch),1);
+    V_In = sdpvar(length(Input_BT),1);
+    V_Out = sdpvar(length(Output_BT),1);
+
+    %%%%%%%%%%%%%%%%%%% solve with yalmip %%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
+    Cost = sum(220*V_In(1) + 2*V_In(1)^2 + 100*V_In(2) + V_In(2)^2); %Vin的次序也是按BT编号由小到大，如本例中1-W 4-Gas
+    Cons = [
+            [X;Y;Z]*V == [V_In;V_Out;zeros(length(Z(:,1)),1)];
+            V_Out >= Demand(:,hour);
+            V_In >=0; V_Out >=0; V>=0;
+            V <= Branch(:,cap);
+            ];
+    ops = sdpsettings('solver','gurobi','verbose',1);
+    solvesdp(Cons,Cost,ops);
+    
+    result(hour).V_In = double(V_In);
+    result(hour).Cost = double(Cost);
+    result(hour).V = double(V);
+    result(hour).Demand = Demand(:,hour);
+    result(hour).V_Out = double(V_Out);
+    Elec_In(1,hour) = result(hour).V_In(1);
+    Gas_In(1,hour) = result(hour).V_In(2);
 end
 
-% Construct X and Y
-Input_BT = unique(Branch(Branch(:,s)==-1,BT));
-Output_BT = unique(Branch(Branch(:,t)==0,BT));
+%% plot the optimal input for each hour
+figure(2)
+plot(Time,Elec_In);
+hold on
+plot(Time,Gas_In);
+legend('Elec Input','Gas Input')
 
-X = zeros(length(Input_BT), Branch_Num);
-Y = zeros(length(Output_BT), Branch_Num);
 
-for i = 1:length(Input_BT)
-    X(i,intersect(Branch(Branch(:,s)==-1),Branch(Branch(:,BT)== Input_BT(i)))) = 1;
-end
-
-for i = 1:length(Output_BT)
-    Y(i,intersect(Branch(Branch(:,t)==0),Branch(Branch(:,BT)== Output_BT(i)))) = 1;
-end
-
-V = sdpvar(length(Branch),1);
-V_In = sdpvar(length(Input_BT),1);
-V_Out = sdpvar(length(Output_BT),1);
-
-%%%%%%%%%%%%%%%%%%% solve with yalmip %%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
-Cost = sum(220*V_In(1) + 2*V_In(1)^2 + 100*V_In(2) + V_In(2)^2); %Vin的次序也是按BT编号由小到大，如本例中1-W 4-Gas
-Cons = [
-        [X;Y;Z]*V == [V_In;V_Out;zeros(length(Z(:,1)),1)];
-        V_Out >= Demand;
-        V_In >=0; V_Out >=0; V>=0;
-        V <= Branch(:,cap);
-        ];
-ops = sdpsettings('solver','gurobi','verbose',1);
-solvesdp(Cons,Cost,ops);
-
-result.V = double(V)
-result.Cost = double(Cost)
 
     
--- read_papaer_v2.m
+++ read_papaer_v2.m
@@ -1,41 +1,59 @@
-Demand = [100;50;30]; % Demand of W,R,Q
-Vin_W1 = sdpvar;
-Vin_W2 = sdpvar;
-Vin_F1 = sdpvar;
-Vin_F2 = sdpvar;
-Vout_W1 = sdpvar;
-Vout_W2 = sdpvar;
-Vout_R1 = sdpvar;
-Vout_R2 = sdpvar;
-Vout_Q1 = sdpvar;
-Vout_Q2 = sdpvar;
-Vout_Q3 = sdpvar;
-V = sdpvar(11,1);
+Time = 1:24;
+          %1    2   3   4   5   6   7   8   9   10  11  12  13  14  15  16
+          %17   18  19  20  21  22  23  24
+Solar  = [ 0,   0,  0,  0,  0,  3,  5,  16, 26, 34, 38, 40, 42, 40, 36, 26,...
+           12,  4,  1,  0,  0,  0,  0,  0;
+          ]*2.5;
+Demand = [45,   47, 46, 44, 45, 53, 56, 77, 80, 92, 95, 94, 85, 83, 84, 82,...
+          89,   101,110,115,110,90, 70, 50;   % W (electricity)
+          17,   16, 16, 16, 16, 18, 20, 25, 25, 27, 30, 37, 35, 34, 33, 32....
+          30,   29, 29, 29, 27, 23, 23, 24;   % R (cold)
+          30,   30, 30, 30, 32, 32, 36, 40, 42, 35, 32, 28, 16, 17, 21, 23,...
+          28,   32, 25, 26, 27, 30, 32, 35;   % Q (head)
+          ]; 
+Gas_In = zeros(1,24);
+Elec_In = zeros(1,24);
 
-Cons = [
-  Vin_W1 == Vout_W1; 
-  Vout_W1 == V(1,1);
-  Vin_W2 == V(2,1);
-  Vin_F1 == V(3,1);
-  Vin_F2 == V(4,1);
-  V(5,1) + V(6,1) + V(7,1) == V(3,1)*0.5;
-  V(8,1) + V(9,1) == 0.25 * V(3,1);
-  V(10,1) + V(11,1) == 0.8 * V(4,1);
-  Vout_R1 == V(2,1)*1.2 + V(5,1)*1.2;
-  Vout_R2 == V(9,1) * 0.9;
-  Vout_Q3 == V(7,1)*0.3;
-  Vout_W2 == V(6,1);
-  Vout_Q1 == V(8,1);
-  Vout_Q2 == V(11,1); V>=0;
-  Vout_W1 + Vout_W2 >= Demand(1);
-  Vout_R1 + Vout_R2 >= Demand(2);
-  Vout_Q1 + Vout_Q2 + Vout_Q3 >= Demand(3);
-];
-Cost = 220*(Vin_W1+Vin_W2) + 2*(Vin_W1+Vin_W2)^2 + 100*(Vin_F1 + Vin_F2) + (Vin_F1 + Vin_F2)^2;
+for hour = 1:24
+    Vin_W1 = sdpvar;
+    Vin_W2 = sdpvar;
+    Vin_F1 = sdpvar;
+    Vin_F2 = sdpvar;
+    Vout_W1 = sdpvar;
+    Vout_W2 = sdpvar;
+    Vout_R1 = sdpvar;
+    Vout_R2 = sdpvar;
+    Vout_Q1 = sdpvar;
+    Vout_Q2 = sdpvar;
+    Vout_Q3 = sdpvar;
+    V = sdpvar(11,1);
 
-ops = sdpsettings('solver','gurobi','verbose',1);
-solvesdp(Cons,Cost,ops);
+    Cons = [
+      Vin_W1 == Vout_W1; 
+      Vout_W1 == V(1,1);
+      Vin_W2 == V(2,1);
+      Vin_F1 == V(3,1);
+      Vin_F2 == V(4,1);
+      V(5,1) + V(6,1) + V(7,1) == V(3,1)*0.5;
+      V(8,1) + V(9,1) == 0.25 * V(3,1);
+      V(10,1) + V(11,1) == 0.8 * V(4,1);
+      Vout_R1 == V(2,1)*1.2 + V(5,1)*1.2;
+      Vout_R2 == V(9,1) * 0.9;
+      Vout_Q3 == V(7,1)*0.3;
+      Vout_W2 == V(6,1);
+      Vout_Q1 == V(8,1);
+      Vout_Q2 == V(11,1); V>=0;
+      Vout_W1 + Vout_W2 >= Demand(1,hour);
+      Vout_R1 + Vout_R2 >= Demand(2,hour);
+      Vout_Q1 + Vout_Q2 + Vout_Q3 >= Demand(3,hour);
+    ];
+    Cost = 220*(Vin_W1+Vin_W2) + 2*(Vin_W1+Vin_W2)^2 + 100*(Vin_F1 + Vin_F2) + (Vin_F1 + Vin_F2)^2;
 
-result.Cost = double(Cost)
-result.V = double(V)
-% 5.0416667 * 10^4
+    ops = sdpsettings('solver','gurobi','verbose',1);
+    solvesdp(Cons,Cost,ops);
+
+    Elec_In(hour) = double(Vin_W1+Vin_W2);
+    Gas_In(hour) = double(Vin_F1+Vin_F2);
+    result(hour).Cost = double(Cost);
+    result(hour).V = double(V);
+end
